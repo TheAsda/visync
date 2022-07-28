8ba6e02a1896a721a8f43c4625b689cdc5ccf286
Kiselev Andrey <omega-faworit@yandex.ru>
2022-07-28 20:31:44 +0300

    feat: update server build

--- packages/server/src/routes/log.ts
+++ packages/server/src/routes/log.ts
@@ -4,20 +4,19 @@ import { logger } from '../logger';
 
 export const logRoutes: FastifyPluginAsync = async (fastify) => {
   fastify.post('/log', (request, reply) => {
-    const body = request.body as LogRequest;
-    const log = loggers[body.level] ?? loggers.info;
+    // const body = request.body as LogRequest;
+    // const log = loggers[body.level] ?? loggers.info;
 
-    log(body.message, { meta: body.meta, app: body.app });
+    // log(body.message, { meta: body.meta, app: body.app });
 
     reply.status(201).send();
   });
 };
 
 const loggers = {
-  trace: logger.trace.bind(logger),
+  trace: logger.log.bind(logger),
   debug: logger.debug.bind(logger),
   info: logger.info.bind(logger),
   warn: logger.warn.bind(logger),
   error: logger.error.bind(logger),
-  fatal: logger.fatal.bind(logger),
 };
