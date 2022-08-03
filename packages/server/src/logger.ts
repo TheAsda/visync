28efc0afb06dcb26b90e91c891925e7650b90cc0
Kiselev Andrey <omega-faworit@yandex.ru>
2022-08-03 11:29:45 +0300

    feat: update logger

--- packages/server/src/logger.ts
+++ packages/server/src/logger.ts
@@ -1,7 +1,18 @@
-import { createLogger } from 'winston';
+import { createLogger, format } from 'winston';
 import { Console } from 'winston/lib/winston/transports';
 
 export const logger = createLogger({
   level: 'info',
-  transports: [new Console()],
+  transports: [
+    new Console({
+      format: format.combine(
+        format.colorize(),
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf(
+          ({ timestamp, level, message }) =>
+            `${timestamp} [${level}]: ${message}`
+        )
+      ),
+    }),
+  ],
 });
