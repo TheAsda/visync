8dc6902cda1ad594ede559898aaa8b4fbfea660c
Kiselev Andrey <omega-faworit@yandex.ru>
2022-12-03 16:38:10 +0300

    feat: add protocol env

--- packages/extension/src/background/url.ts
+++ packages/extension/src/background/url.ts
@@ -1,5 +1,6 @@
+export const serverProtocol = process.env.SERVER_PROTOCOL ?? 'http';
 export const serverHostname = process.env.SERVER_HOSTNAME ?? '127.0.0.1';
 export const serverPort = process.env.SERVER_PORT ?? 7001;
-export const serverUrl = `http://${serverHostname}:${serverPort}`;
+export const serverUrl = `${serverProtocol}://${serverHostname}:${serverPort}`;
 
 export const getUrl = (path: string) => new URL(path, serverUrl);
